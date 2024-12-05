697ff1d1e9ae0d1e7fada8042606e784ec4028db
Rosalie Perron <gritty.joy9372@fastmail.com>
2024-12-04 20:46:42 -0500

    Day 2

---
+++ day_2/s2.py
@@ -0,0 +1,60 @@
+import csv 
+
+
+INPUT_FILE = "input_s1"
+
+
+def read_input():
+    result = []
+
+    with open(INPUT_FILE, "r") as f:
+        reader = csv.reader(f, delimiter=" ")
+        for row in reader:
+            result.append([])
+
+            for el in row:
+                result[-1].append(int(el))
+
+    return result
+
+
+def verify_increases(line):
+    return all([line[i] < line[i+1] for i in range(len(line) - 1)])
+
+
+def verify_gaps(line):
+    return all([1 <= abs(line[i+1] - line[i]) <= 3 for i in range(len(line) - 1)])
+
+
+def is_safe(line):
+    reversed_line = line.copy()
+    reversed_line.reverse()
+
+    return (
+        verify_increases(line) or verify_increases(reversed_line)
+    ) and verify_gaps(line)
+
+
+def verify_line(line):
+    if is_safe(line):
+        return True
+
+    for i, _ in enumerate(line):
+        if is_safe(line[:i] + line[i+1:]):
+            return True
+
+
+def verify_report():
+    report = read_input()
+    result = 0
+
+    for line in report:
+        safe = verify_line(line)
+        if safe:
+            result += 1
+
+    return result
+
+
+if __name__ == "__main__":
+    print(verify_report())
